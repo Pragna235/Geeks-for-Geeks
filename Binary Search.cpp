3e8454b2d248f04c4b064d1bfe192cdc19989ebb
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2023-07-17 13:37:58 +0530

    Create Binary Search.cpp

---
+++ Binary Search.cpp
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    int binarysearch(int arr[], int n, int k) {
+        // code here
+        int left=0,right=n-1;
+        while(left<=right){
+            int mid = left + (right-left)/2;
+            if(arr[mid]==k)
+            return mid;
+            else if(arr[mid]<k)
+            left=mid+1;
+            else
+            right=mid-1;
+            
+            
+        }
+        return -1;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int N;
+        cin >> N;
+        int arr[N];
+        for (int i = 0; i < N; i++) cin >> arr[i];
+        int key;
+        cin >> key;
+        Solution ob;
+        int found = ob.binarysearch(arr, N, key);
+        cout << found << endl;
+    }
+}
+
+// } Driver Code Ends
