2ffdf04b46f3f81d7a141c2d6224a9eeee57b326
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2024-02-07 22:43:50 +0530

    Create Count Digits.cpp

---
+++ Count Digits.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    int evenlyDivides(int n){
+        //code here
+        int temp=n,count=0;
+        while(temp!=0){
+            int d = temp%10;
+            temp/=10;
+            
+            if(d>0 && n%d==0){
+                count++;
+    
+            }
+        }
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.evenlyDivides(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
