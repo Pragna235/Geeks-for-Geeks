2f335252f27d078e4d7f7c2e5b70980d9a033ede
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2024-05-01 22:42:54 +0530

    Create Arrange consonants and vowels.cpp

---
+++ Arrange consonants and vowels.cpp
@@ -0,0 +1,90 @@
+geeksforgeeks
+Courses
+On Offers
+Tutorials
+Jobs
+Practice
+Contests
+P
+
+
+Arrange Consonants and Vowels
+MediumAccuracy: 49.98%Submissions: 32K+Points: 4
+ Done with winning Geekbits? Now win GfG Bag, GfG T-shirt & much more just by writing your experiences. Start Writing, Start Winning.
+banner
+Given a singly linked list having n nodes containing english alphabets ('a'-'z'). Rearrange the linked list in such a way that all the vowels come before the consonants while maintaining the order of their arrival. 
+
+Example 1:
+
+Input:
+n = 9
+linked list: a -> b -> c -> d -> e -> f -> g -> h -> i 
+Output: 
+a -> e -> i -> b -> c -> d -> f -> g -> h
+Explanation: 
+After rearranging the input linked list according to the condition the resultant linked list will be as shown in output.
+Example 2:
+
+Input:
+n = 8
+linked list: a -> b -> a -> b -> d -> e -> e -> d 
+Output: 
+a -> a -> e -> e -> b -> b -> d -> d
+Explanation: 
+After rearranging the input linked list according to the condition the resultant linked list will be as shown in output.
+Your Task:
+Your task is to complete the function arrangeCV(), which takes head of linked list and arranges the list in such a way that all the vowels come before the consonants while maintaining the order of their arrival and returns the head of the updated linked list.
+
+Expected Time Complexity :  O(n)
+Expected Auxiliary Space :  O(1)
+
+Constraints:
+1 <= n <= 104
+'a' <= elements of linked list <= 'z'
+
+Seen this question in a real interview before ?
+1/4
+Yes
+No
+Company Tags
+Topic Tags
+If you are facing any issue on this page. Please let us know.
+Output Window
+Problem Solved Successfully
+Suggest Feedback
+Test Cases Passed
+1120 / 1120
+Attempts : Correct / Total
+1 / 1
+Accuracy :
+100%
+Points Scored 
+4 / 4
+Your Total Score:
+105 
+Time Taken
+0.16
+Solve Next
+
+Linked List in Zig-Zag fashion
+Rearrange linked list in-place
+Move all zeros to the front of the linked list
+
+C++ (g++ 5.4)
+
+
+
+    return start;
+
+
+ 
+
+Custom Input
+Congratulations !
+You just earned 1 Geekbit for solving today’s POTD.
+gfg_coin
+streak-imgCurrent streak : 1 day
+Keep up your streak to earn more. See you tomorrow !
+banner
+Get a chance to call HR directly. Sign up on Indeed, our POTD partner. 
+
