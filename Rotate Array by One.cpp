fe00cf3f3469135d05f880ba0abe3541f9c9c461
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2024-08-05 18:59:10 +0530

    Create Rotate Array by One.cpp

---
+++ Rotate Array by One.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    void rotate(vector<int> &arr) {
+        // code here
+        int n = arr.size();
+        int temp = arr[n-1];
+        
+        for(int i=n-2;i>=0;i--){
+            arr[i+1] = arr[i];
+        }
+        
+        arr[0] = temp;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        ob.rotate(arr);
+        for (long long i = 0; i < arr.size(); i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
