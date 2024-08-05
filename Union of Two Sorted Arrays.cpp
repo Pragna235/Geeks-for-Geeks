b1362bc01ee5bdd4bca1901d0be436c9b49bdc4b
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2024-08-05 19:41:04 +0530

    Create Union of Two Sorted Arrays.cpp

---
+++ Union of Two Sorted Arrays.cpp
@@ -0,0 +1,37 @@
+#User function Template for python3
+
+class Solution:
+    
+    #Function to return a list containing the union of the two arrays.
+    def findUnion(self,arr1,arr2,n,m):
+        '''
+        :param a: given sorted array a
+        :param n: size of sorted array a
+        :param b: given sorted array b
+        :param m: size of sorted array b
+        :return:  The union of both arrays as a list
+        '''
+        # code here 
+        return sorted(set(arr1+arr2))
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# Contributed by : Nagendra Jha
+# Modified by : Sagar Gupta
+
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases) :
+        n,m = map(int,input().strip().split())
+        a = list(map(int,input().strip().split()))
+        b = list(map(int,input().strip().split()))
+        ob=Solution()
+        li = ob.findUnion(a,b,n,m)
+        for val in li:
+            print(val, end = ' ')
+        print()
+# } Driver Code Ends
