6545b0f340e867ce95acda5fe17202183eae2a6b
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2024-08-05 22:04:31 +0530

    Create Find the element that appears once.cpp

---
+++ Find the element that appears once.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    int search(int n, int arr[]) {
+        // code
+        int xorr=0;
+        for(int i=0;i<n;i++){
+            xorr=xorr^arr[i];
+        }
+        return xorr;
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver program
+int main() {
+    int t, len;
+    cin >> t;
+    while (t--) {
+        cin >> len;
+        int arr[len];
+        for (int i = 0; i < len; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        cout << ob.search(len, arr) << '\n';
+    }
+    return 0;
+}
+
+// } Driver Code Ends
