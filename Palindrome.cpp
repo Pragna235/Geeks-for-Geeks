57304c3ddda19e0eb5ba29f098bef2277cdd9e94
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2024-08-08 11:51:19 +0530

    Create Palindrome.cpp

---
+++ Palindrome.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+	public:
+		string is_palindrome(int n)
+		{
+		    // Code here.
+		    int dup=n;
+		    int rev=0;
+		    while(n){
+		        int rem = n%10;
+		        rev = (rev*10)+rem;
+		        n/=10;
+		    }
+		    if(dup==rev) return "Yes";
+		    return "No";
+		}
+};
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n;
+    	cin >> n;
+    	Solution ob;
+    	string ans = ob.is_palindrome(n);
+    	cout << ans <<"\n";
+    }
+	return 0;
+}
+
+// } Driver Code Ends
