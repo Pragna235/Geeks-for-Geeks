555cdd0b9d8c1a1c06be158623455a6205f250b1
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2024-08-10 17:28:56 +0530

    Create Rearrange an array with O(1) extra space.cpp

---
+++ Rearrange an array with O(1) extra space.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+    
+
+class Solution{
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to rearrange an array so that arr[i] becomes arr[arr[i]]
+    //with O(1) extra space.
+    void arrange(long long arr[], int N) {
+        // Your code here
+        for(int i=0;i<N;i++){
+            arr[i] += (arr[arr[i]] % N) * N;
+        }
+        for(int i=0;i<N;i++){
+            arr[i]/=N;
+        }
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    
+    int t;
+    //testcases
+    cin>>t;
+    while(t--){
+        
+        int n;
+        //size of array
+        cin>>n;
+        long long A[n];
+        
+        //adding elements to the array
+        for(int i=0;i<n;i++){
+            cin>>A[i];
+        }
+        Solution ob;
+        //calling arrange() function
+        ob.arrange(A, n);
+        
+        //printing the elements 
+        for(int i=0;i<n;i++){
+            cout << A[i]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
