caea2e9892c114b6b97186ebbadae0dc359b8fc3
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2024-08-12 09:14:10 +0530

    Create Search Pattern (KMP-Algorithm).cpp

---
+++ Search Pattern (KMP-Algorithm).cpp
@@ -0,0 +1,106 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+        
+        void computeLps(string &pat,int M, vector<int> &lps){
+            int len = 0;
+
+    // lps[0] is always 0
+    lps[0] = 0;
+
+    // loop calculates lps[i] for i = 1 to M-1
+    int i = 1;
+    while (i < M) {
+        if (pat[i] == pat[len]) {
+            len++;
+            lps[i] = len;
+            i++;
+        }
+        else // (pat[i] != pat[len])
+        {
+            if (len != 0) {
+                len = lps[len - 1];
+            }
+            else // if (len == 0)
+            {
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
+        }
+        vector <int> search(string pat, string txt)
+        {
+            //code here
+            int M = pat.length();
+    int N = txt.length();
+
+    // Create lps[] that will hold the longest prefix suffix
+    // values for pattern
+    vector<int> lps(M);
+
+    vector<int> result;
+
+    // Preprocess the pattern (calculate lps[] array)
+    computeLps(pat, M, lps);
+
+    int i = 0; // index for txt
+    int j = 0; // index for pat
+    while ((N - i) >= (M - j)) {
+        if (pat[j] == txt[i]) {
+            j++;
+            i++;
+        }
+
+        if (j == M) {
+            result.push_back(i - j + 1);
+            j = lps[j - 1];
+        }
+
+        // Mismatch after j matches
+        else if (i < N && pat[j] != txt[i]) {
+
+            // Do not match lps[0..lps[j-1]] characters,
+            // they will match anyway
+            if (j != 0)
+                j = lps[j - 1];
+            else
+                i = i + 1;
+        }
+            }
+            return result;
+            
+        }
+     
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string S, pat;
+        cin >> S >> pat;
+        Solution ob;
+        vector <int> res = ob.search(pat, S);
+        if (res.size()==0)
+            cout<<-1<<endl;
+        else {
+            for (int i : res) cout << i << " ";
+            cout << endl;
+        }
+    }
+    return 0;
+}
+
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
