48e6dd0d58377c031b7d026f29808eb5e8ea9e54
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2024-09-01 17:41:49 +0530

    Create Search in a row-column sorted Matrix3.cpp

---
+++ Search in a row-column sorted Matrix3.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+  public:
+    // Function to search a given number in row-column sorted matrix.
+    bool search(vector<vector<int> > &matrix, int n, int m, int x) {
+        // code here
+        int row=0,col=m-1;
+        while(row<n && col>=0){
+            if(matrix[row][col] == x) return true;
+            else if(matrix[row][col] < x) row++;
+            else col--;
+        }
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int> > matrix(n);
+
+        for (int i = 0; i < n; i++) {
+            matrix[i].assign(m, 0);
+            for (int j = 0; j < m; j++) {
+                cin >> matrix[i][j];
+            }
+        }
+
+        int x;
+        cin >> x;
+        Solution obj;
+        if (obj.search(matrix, n, m, x))
+            cout << "1\n";
+        else
+            cout << "0\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
