26a1279a9e535de5d94316e50694f425855f08ec
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2024-09-02 13:28:20 +0530

    Create Kth from End of Linked List.cpp

---
+++ Kth from End of Linked List.cpp
@@ -0,0 +1,106 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printList(Node* node) {
+    while (node != NULL) {
+        cout << node->data << " ";
+        node = node->next;
+    }
+    cout << "\n";
+}
+
+
+// } Driver Code Ends
+/* struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};*/
+
+// Function to find the data of kth node from the end of a linked list.
+class Solution {
+  public:
+    int lengthList(Node *head){
+        int cnt=0;
+        Node* temp = head;
+        while(temp){
+            cnt++;
+            temp=temp->next;
+        }
+        return cnt;
+    }
+    int getKthFromLast(Node *head, int k) {
+        // Your code here
+        int n = lengthList(head); //9
+        if(k>n) return -1;
+        int cnt = n+1;
+        Node* temp = head;
+        while(temp){
+            cnt--;
+            if(k==cnt){
+                return temp->data;
+            }
+            temp=temp->next;
+        }
+        
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+
+        // Read numbers from the input line
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        int x;
+        cin >> x;
+        cin.ignore();
+
+        Node* head = nullptr;
+
+        // Check if the array is empty
+        if (!arr.empty()) {
+            head = new Node(arr[0]);
+            Node* tail = head;
+            for (size_t i = 1; i < arr.size(); ++i) {
+                tail->next = new Node(arr[i]);
+                tail = tail->next;
+            }
+        }
+
+        Solution ob;
+        cout << ob.getKthFromLast(head, x) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
