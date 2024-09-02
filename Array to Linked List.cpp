ccca3ccce5caa11f141cd86bf9f56920c6fd40cf
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2024-09-02 12:04:20 +0530

    Create Array to Linked List.cpp

---
+++ Array to Linked List.cpp
@@ -0,0 +1,90 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+  public:
+    int data;
+    Node* next;
+
+    // Default constructor
+    Node() {
+        data = 0;
+        next = NULL;
+    }
+
+    // Parameterised Constructor
+    Node(int data) {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+/*class Node {
+public:
+    int data;
+    Node* next;
+
+    // Default constructor
+    Node()
+    {
+        data = 0;
+        next = NULL;
+    }
+
+    // Parameterised Constructor
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};*/
+
+class Solution {
+  public:
+    Node* constructLL(vector<int>& arr) {
+        // code here
+        Node* head = new Node(arr[0]);
+        Node* mover = head;
+        for(int i=1;i<arr.size();i++){
+            Node* temp = new Node(arr[i]);
+            mover->next = temp;
+            mover = temp;
+        }
+        return head;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        string line;
+        getline(cin, line);
+        stringstream ss(line);
+        vector<int> arr;
+        int num;
+        while (ss >> num) {
+            arr.push_back(num);
+        }
+        Solution ob;
+        Node* ans = ob.constructLL(arr);
+        while (ans) {
+            cout << ans->data << " ";
+            ans = ans->next;
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
