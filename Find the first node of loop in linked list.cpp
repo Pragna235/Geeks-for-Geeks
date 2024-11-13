00a62b72b0e8015113e8571033f8eeb1f01d4c15
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2024-11-13 05:43:29 +0530

    Create Find the first node of loop in linked list.cpp

---
+++ Find the first node of loop in linked list.cpp
@@ -0,0 +1,102 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printList(Node *node) {
+    while (node != NULL) {
+        cout << node->data << " ";
+        node = node->next;
+    }
+    cout << "\n";
+}
+
+void loopHere(Node *head, Node *tail, int position) {
+    if (position == 0)
+        return;
+
+    Node *walk = head;
+    for (int i = 1; i < position; i++)
+        walk = walk->next;
+    tail->next = walk;
+}
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+/*struct Node
+{
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+
+*/
+class Solution {
+  public:
+    // Function to find first node if the linked list has a loop.
+    int findFirstNode(Node* head) {
+        // your code here
+        Node* slow=head;
+        Node* fast = head;
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast){
+                slow=head;
+                while(slow!=fast){
+                    slow=slow->next;
+                    fast=fast->next;
+                }
+                return slow->data;
+            }
+        }
+        return -1;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int k;
+        cin >> k;
+        cin.ignore();
+        struct Node *head = new Node(arr[0]);
+        struct Node *tail = head;
+        for (int i = 1; i < arr.size(); ++i) {
+            tail->next = new Node(arr[i]);
+            tail = tail->next;
+        }
+        loopHere(head, tail, k);
+
+        Solution ob;
+        int ans = ob.findFirstNode(head);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
