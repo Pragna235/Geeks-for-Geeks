d9180a2453ca9a62381134f2888db1787e8fc98d
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2024-11-13 13:39:51 +0530

    Create Add 1 to a Linked List Number.cpp

---
+++ Add 1 to a Linked List Number.cpp
@@ -0,0 +1,106 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printList(Node* node) {
+    while (node != NULL) {
+        cout << node->data % 10;
+        node = node->next;
+    }
+    cout << "\n";
+}
+
+
+// } Driver Code Ends
+// User function template for C++
+
+/*
+
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+int helper(Node* temp){
+    if(temp==NULL) return 1;
+    int carry = helper(temp->next);
+    temp->data += carry;
+    if(temp->data<10){
+        return 0;
+    }
+    temp->data =0;
+    return 1;
+}
+class Solution {
+  public:
+    Node* addOne(Node* head) {
+        // Your Code here
+        // return head of list after adding one
+        Node* temp = head;
+        int carry = helper(temp);
+        if(carry==1){
+            Node* newNode = new Node(1);
+            newNode->next = head;
+            head = newNode;
+        }
+        return head;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        if (arr.empty()) {
+            cout << -1 << endl;
+            continue;
+        }
+
+        int data = arr[0];
+        struct Node* head = new Node(data);
+        struct Node* tail = head;
+        for (int i = 1; i < arr.size(); ++i) {
+            data = arr[i];
+            tail->next = new Node(data);
+            tail = tail->next;
+        }
+        Solution ob;
+        head = ob.addOne(head);
+        printList(head);
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
